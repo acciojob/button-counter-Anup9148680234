674dd8257abba4adaec82cc0f5f779a79db8486f
Anup Angadi <52240005+Anup9148680234@users.noreply.github.com>
2023-05-16 16:56:26 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-
+import {useState} from "react";
 import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [a, setA] = useState(0);
+
+  function addA(){
+    setA(a+1);
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>Button clicked {a} times</p>
+      <button onClick={addA}>Click me</button>
     </div>
   )
 }
 
 export default App
+
